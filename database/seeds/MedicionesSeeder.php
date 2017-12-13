9620bb47a94eaeb1c9b57684107e10b5efa05ae3
fr4j4 <fgonz@outlook.com>
2017-12-13 12:46:51 -0300

    lecturas de prueba

--- database/seeds/MedicionesSeeder.php
+++ database/seeds/MedicionesSeeder.php
@@ -12,11 +12,11 @@ class MedicionesSeeder extends Seeder{
      */
     public function run(){
     	$faker = Faker\Factory::create();
-        
+        $lectores_id=["A001F43","A001F20","A001F93","A001F68","A001F70","A001G00"];
 
     	for ($i=0; $i <10000; $i++) { 
     		$lecture=new Lecture();
-    		$lecture->client_id="A001F43";
+    		$lecture->client_id=array_rand($lectores_id);
     		$lecture->created_at=$faker->dateTimeBetween($startDate = '-10 months', $endDate = 'now');
     		$lecture->save();
     		
