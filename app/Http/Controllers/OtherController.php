56ad2f2b26be83b22e550e76baff3b6594f1c4cb
fr4j4 <fgonz@outlook.com>
2017-12-14 11:09:52 -0300

    registro

---
+++ app/Http/Controllers/OtherController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+namespace App\Http\Requests\NewLectorRegister;
+
+use Illuminate\Http\Request;
+
+class OtherController extends Controller{
+    public function registrar_lector(NewLectorRegister $req){
+    	
+    }
+}
--- database/seeds/MedicionesSeeder.php
+++ database/seeds/MedicionesSeeder.php
@@ -14,7 +14,7 @@ class MedicionesSeeder extends Seeder{
     	$faker = Faker\Factory::create();
         $lectores_id=["A001F43","A001F20","A001F93","A001F68","A001F70","A001G00"];
 
-    	for ($i=0; $i <10000; $i++) { 
+    	for ($i=0; $i <1000; $i++) { 
     		$lecture=new Lecture();
     		$lecture->client_id=$lectores_id[array_rand($lectores_id)];
     		$lecture->created_at=$faker->dateTimeBetween($startDate = '-10 months', $endDate = 'now');
